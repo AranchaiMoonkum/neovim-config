51ccc52463e6b7cae06b6c383ca89c7aeea59f00
Aranchai Moonkum <aranchai@Aranchais-MacBook-Pro.local>
2025-02-02 00:05:37 +0700

    feat: add carbide theme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,4 +3,12 @@ return {
         "lewis6991/gitsigns.nvim",
         config = function() require "gitsigns".setup {} end,
     },
+
+    -- themes
+    {
+        "ferdinandrau/carbide.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function() require "plugins.themes.carbide" end,
+    },
 }
