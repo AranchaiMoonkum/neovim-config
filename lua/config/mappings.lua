451a9bbc4331310100b4aaa1046c205cb0982736
Aranchai Moonkum <aranchai@Aranchais-MacBook-Pro.local>
2025-02-07 01:56:39 +0700

    added a keymap for neotree

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -5,3 +5,6 @@ vim.keymap.set("i", "jk", "<ESC>", opts)
 
 -- telescope
 vim.keymap.set("n", "<leader>ff", "<CMD>Telescope find_files<CR>", opts)
+
+-- explorer
+vim.keymap.set("n", "<leader>e", "<CMD>Neotree<CR>", opts)
